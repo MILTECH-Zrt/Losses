b800e2e76332f1896a9c5299f45754df10dade87
Rembeczki Szabolcs <rembeczki.szabolcs@miltech.hu>
2025-02-26 08:42:00 +0100

    Első verzió

---
+++ losscalc.py
@@ -0,0 +1,178 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+eps0 = 8.8541878188e-12  # vacuum permitivity, F/m
+mu0 = 1.25663706127e-6  # vacuum permeability, N/A2
+c = 299792458  # lightspeed m/s
+
+"""Waveguide material conductivity in Siemens/m"""
+WG_conductivity = {
+    "Aluminum": 37.67e6,
+    "Brass (5% Zn)":    33.4e6,
+    "Brass (30% Zn)":   16.7e6,
+    "Copper":   59.6e6,
+    "Copper (annealed)":     58e6,
+    "Gold":             41.1e6,
+    "Silver":   63e6,
+}
+
+"""Waveguide type and a,b sizes in inch, recommended frequency range"""
+WG_types = {
+    "WR2300": [23, 11.5, '0.32 to 0.45 GHz'],
+    "WR2100": [21, 10.5, '0.35 to 0.50 GHz'],
+    "WR1800": [18, 9, '0.45 to 0.63 GHz'],
+    "WR1500": [15, 7.5, '0.50 to 0.75 GHz'],
+    "WR1150": [11.5, 5.75, '0.63 to 0.97 GHz'],
+    "WR975": [9.75, 4.875, '0.75 to 1.15 GHz'],
+    "WR770": [7.7, 3.85, '0.97 to 1.45 GHz'],
+    "WR650": [6.5, 3.25, '1.15 to 1.72 GHz'],
+    "WR510": [5.1, 2.55, '1.45 to 2.20 GHz'],
+    "WR430": [4.3, 2.15, '1.72 to 2.60 GHz'],
+    "WR340": [3.4, 1.7, '2.20 to 3.30 GHz'],
+    "WR284": [2.84, 1.34, '2.60 to 3.95 GHz'],
+    "WR229": [2.29, 1.145, '3.30 to 4.90 GHz'],
+    "WR187": [1.872, 0.872, '3.95 to 5.85 GHz'],
+    "WR159": [1.59, 0.795, '4.90 to 7.05 GHz'],
+    "WR137": [1.372, 0.622, '5.85 to 8.20 GHz'],
+    "WR112": [1.122, 0.497, '7.05 to 10 GHz'],
+    "WR102": [1.02, 0.51, '7.00 to 11 GHz'],
+    "WR90": [0.9, 0.4, '8.20 to 12.40 GHz'],
+    "WR75": [0.75, 0.375, '10.00 to 15 GHz'],
+    "WR62": [0.622, 0.311, '12.40 to 18 GHz'],
+    "WR51": [0.51, 0.255, '15.00 to 22 GHz'],
+    "WR42": [0.42, 0.17, '18.00 to 26.50 GHz'],
+    "WR34": [0.34, 0.17, '22.00 to 33 GHz'],
+    "WR28": [0.28, 0.14, '26.50 to 40 GHz'],
+    "WR22": [0.224, 0.112, '33.00 to 50 GHz'],
+    "WR19": [0.188, 0.094, '40.00 to 60 GHz'],
+    "WR15": [0.148, 0.074, '50.00 to 75 GHz'],
+    "WR12": [0.122, 0.061, '60 to 90 GHz'],
+    "WR10": [0.1, 0.05, '75 to 110 GHz'],
+    "WR8": [0.08, 0.04, '90 to 140 GHz'],
+    "WR6": [0.065, 0.0325, '110 to 170 GHz'],
+    "WR7": [0.065, 0.0325, '110 to 170 GHz'],
+    "WR5": [0.051, 0.0255, '140 to 220 GHz'],
+    "WR4": [0.043, 0.0215, '172 to 260 GHz'],
+    "WR3": [0.034, 0.017, '220 to 330 GHz']
+    }
+
+""" Unit conversion: Y[in SI unit] = Y * unit2SI[from unit str]"""
+unit2SI = {
+        'fm': 1e-15,
+        'pm': 1e-12,
+        'angstrom': 1e-10,
+        'nm': 1e-9,
+        'um': 1e-6,
+        'mm': 1e-3,
+        'cm': 1e-2,
+        'dm': 1e-1,
+        'km': 1000,
+        'm': 1,
+        'nmi': 1852,
+        'mi': 1609.344,
+        'ft': 0.3048,
+        'kft': 304.8,
+        'inch': 0.0254,
+        'W': 1,
+        'kW': 1000,
+        'MW': 1e6,
+        'hp': 735.49875,  # horsepower in W
+        'GHz': 1e9,
+        'MHz': 1e6,
+        'kHz': 1000,
+        'Hz': 1,
+        'ms': 1e-3,
+        's': 1,
+        'us': 1e-6,
+        'm/s': 1,
+        'km/h': 0.2778,
+        'mph': 0.4470,
+        'mach': 340.29,
+        'rpm': 60,
+        'deg/s': 360,
+        'rad/s': 2 * np.pi,
+        'm/s2': 1,
+        'g': 0.101972,  # 1/9.80665, standard gravity (see Acceleration - Wiki)
+        'ft/s2': 3.28084,
+        'J': 1,  # Joule
+        'kJ': 1000,
+        'MJ': 1000000,
+        'kWh': 3600000,
+        'BTU': 1055.05585262,
+        'eV': 1.602176634e-19,
+        'keV': 1.602176634e-16,
+        'cal': 4.184,  # calorie
+        'kcal': 4184,
+        'Pa': 1,  # Pascal
+        'kPa': 1000,
+        'atm': 101325,
+        'bar': 1e5,
+        'mbar': 100,
+        'hPa': 100,
+        'psi': 6894.75729,
+        'Torr': 133.322368,
+        "c": 299792458,  # lightspeed m/s
+        "k": 1.3806504e-23  # Boltzmann J/K
+    }
+
+
+def rectwaveguide_cutofffreq(a, b, m=1, n=0, mur=1, epsr=1):
+    """
+    Calculates the cut-off frequency of rectangular waveguide
+    :param a: waveguide cross-section longer side length, in m
+    :param b: waveguide cross-section shorter side length, in m
+    :param m: TEmn mode m, integer value of 0,1,2...
+    :param n: TEmn mode n, integer value of 0,1,2...
+    :param mur: relative permeability
+    :param epsr: relative permittivity
+    :return: fc: cut-off frequency of rectangular waveguide, in Hz
+    """
+    kc = np.sqrt((m * np.pi/a)**2 + (n * np.pi/b)**2)  # cutoff wavenumber
+    fc = c * kc / (2 * np.pi * np.sqrt(mur * epsr))  # cutoff frequency in Hz
+    return fc
+
+
+def rectwaveguideloss(a, b, sig, f, fc, waveguidelength=1):
+    """
+    Calculates the specific and total loss of a rectangular waveguide
+    :param a: waveguide cross-section longer side length, in m
+    :param b: waveguide cross-section shorter side length, in m
+    :param sig: sigma conductivity of waveguide inner wall material, in Siemens/m
+    :param f: wave frequency, in Hz
+    :param fc: waveguide cut-off frequency, in Hz
+    :param waveguidelength: length of the waveguide, in m
+    :return:
+    alfa - loss per meter, in dB/m
+    Lwg - loss on waveguide, in dB
+    Ref.: H.Meikle, Modern Radar Systems (2nd.ed.) p.87, eq.(4.4)
+    """
+
+    fr = f/fc  # frequency ratio
+    # attenuation of waveguide in dB/m:
+    if fr**2 >= 1:
+        alfa = np.sqrt((np.pi/(2*sig)) * np.sqrt(eps0/mu0)) * (1/a**1.5) * ((2 * fr**-0.5 + (a/b)*(fr**1.5)) /
+                                                                        np.sqrt(fr**2 - 1)) * 20 * np.log10(np.e)
+
+        # total loss of waveguide:
+        Lwg = waveguidelength * alfa
+    else:
+        alfa = np.nan
+        Lwg = np.nan
+    return alfa, Lwg
+
+
+def plotrectwgatten(a, b, sig, fc, WGname, WGmaterial):
+    """ Plot rectangular waveguide attenuation vs. frequency"""
+    fr = np.arange(2, 10, 0.2)*1e9
+    attr = []
+    for f in fr:
+        att, L = rectwaveguideloss(a, b, sig, f, fc)
+        attr.append(att)
+    fig, ax = plt.subplots()
+    ax.plot(fr/1e9, attr, label=WGname + ", " + WGmaterial)
+    ax.set_xlabel('Frequency [GHz]')
+    ax.set_ylabel('Attenuation [dB/m]')
+    ax.set_title("Rect. Waveguide Attenuation")
+    ax.legend()
+    fig.show()
+
---
+++ lossesgui.py
@@ -0,0 +1,267 @@
+import tkinter as tk
+from tkinter import ttk
+
+import losscalc as LC
+
+""" Init GUI and its main parameters"""
+root = tk.Tk()
+root.title('Loss Calculations - MILTECH Co.')
+root.iconbitmap(r'./Miliconnew.ico')  # use relat
+root.geometry("825x421")  # width * height
+lw = 25  # labelwidth
+ew = 15  # entrywidth
+uw = 5   # unitwidth
+
+""" Tabs """
+tabControl = ttk.Notebook(root)
+
+tabRWG = ttk.Frame(tabControl)
+tabControl.add(tabRWG, text='Rectangular WG', sticky='NSEW')
+
+tabVSWR = ttk.Frame(tabControl)
+tabControl.add(tabVSWR, text='VSWR', sticky='NSEW')
+
+tabControl.grid()  # show tabs
+
+
+""" Events """
+
+
+def rwg_changed(event):
+
+    rwgtype = rwgType_combo.get()
+    aunit = rwgaunits_combo.get()
+    a = LC.WG_types[rwgtype][0] * LC.unit2SI["inch"] / LC.unit2SI[aunit]
+    b = LC.WG_types[rwgtype][1] * LC.unit2SI["inch"] / LC.unit2SI[aunit]
+    dbRWGa.set(a)
+    dbRWGb.set(b)
+    rwgRecomFreq.set(LC.WG_types[rwgType_combo.get()][2])
+
+
+def rwgaunit_changed(event):
+    rwgbunit.set(rwgaunits_combo.get())
+
+
+def rwgMaterial_changed(event):
+    dbRWGcond.set(LC.WG_conductivity[rwgMaterial_combo.get()])
+    calc_rwg()
+
+
+def rwgfrequnit_changed(event):
+    rwgfcunit.set(rwgfunits_combo.get())
+
+
+""" Calculations """
+
+
+def calc_rwg():
+    aunit = rwgaunits_combo.get()
+    a = dbRWGa.get() * LC.unit2SI[aunit]
+    b = dbRWGb.get() * LC.unit2SI[aunit]
+    m = intRWGTEm.get()
+    n = intRWGTEn.get()
+    mur = dbRWGmur.get()
+    epsr = dbRWGepsr.get()
+    sig = dbRWGcond.get()
+    frequnit = rwgfunits_combo.get()
+    fr = dbRWGfr.get() * LC.unit2SI[frequnit]
+    rwgl = dbRWGlength.get() * LC.unit2SI[rwgLunits_combo.get()]
+
+    fc = LC.rectwaveguide_cutofffreq(a, b, m, n, mur, epsr)
+    Alfa, Lrwg = LC.rectwaveguideloss(a, b, sig, fr, fc, rwgl)
+    # alfa, Lwg = LC.rectwaveguideloss(0.072136,0.034036,3.767e7, 2.6e9, 2.079406e9)
+
+    rwgRecomFreq.set(LC.WG_types[rwgType_combo.get()][2])
+    dbCutoffFreq.set(fc/LC.unit2SI[frequnit])
+    dbRWGatten.set(Alfa)
+    dbRWGloss.set(Lrwg)
+
+
+""" VSWR Frame"""
+
+vswframe = ttk.LabelFrame(tabVSWR, text="Parameters")
+vswframe.grid(row=0, column=0, sticky='NW')
+
+rc = 0
+ttk.Label(vswframe, text='VSWR:', width=lw, anchor='w').grid(row=rc, column=0)
+dbVSWR = tk.DoubleVar()
+dbVSWR.set(1)
+VSWR_entry = tk.Entry(vswframe, textvariable=dbVSWR, width=ew)
+VSWR_entry.grid(row=rc, column=1)
+
+
+""" Rectangular Waveguide Frame """
+
+rwgframe = ttk.LabelFrame(tabRWG, text="Parameters")
+rwgframe.grid(row=0, column=0, sticky='NW')
+
+rwgresframe = ttk.LabelFrame(tabRWG, text="Results")
+rwgresframe.grid(row=0, column=1, sticky='NW')
+
+rc = 0
+ttk.Label(rwgframe, text='Frequency:', width=lw, anchor='w').grid(row=rc, column=0)
+dbRWGfr = tk.DoubleVar()
+dbRWGfr.set(1)
+RWGfr_entry = tk.Entry(rwgframe, textvariable=dbRWGfr, width=ew)
+RWGfr_entry.grid(row=rc, column=1)
+
+rwgfunits = ["GHz", "MHz", "kHz", "Hz"]
+rwgfunits_combo = ttk.Combobox(rwgframe, values=rwgfunits, width=uw)
+rwgfunits_combo.current(0)
+rwgfunits_combo.grid(row=rc, column=2)
+rwgfunits_combo.bind("<<ComboboxSelected>>", rwgfrequnit_changed)
+
+rc += 1
+ttk.Label(rwgframe, text='Waveguide type:', width=lw, anchor='w').grid(row=rc, column=0)
+rwgTypes = list(LC.WG_types.keys())
+rwgType_combo = ttk.Combobox(rwgframe, values=rwgTypes, width=ew-3)
+rwgType_combo.current(0)
+rwgType_combo.grid(row=rc, column=1)
+rwgType_combo.bind("<<ComboboxSelected>>", rwg_changed)
+
+rc += 1
+ttk.Label(rwgframe, text='Cross section a:', width=lw, anchor='w').grid(row=rc, column=0)
+dbRWGa = tk.DoubleVar()
+dbRWGa.set(3)
+RWGa_entry = tk.Entry(rwgframe, textvariable=dbRWGa, width=ew)
+RWGa_entry.grid(row=rc, column=1)
+
+rwgaunits = ["mm", "cm", "inch"]
+rwgaunits_combo = ttk.Combobox(rwgframe, values=rwgaunits, width=uw)
+rwgaunits_combo.current(2)
+rwgaunits_combo.grid(row=rc, column=2)
+rwgaunits_combo.bind("<<ComboboxSelected>>", rwgaunit_changed)
+
+rc += 1
+ttk.Label(rwgframe, text='Cross section b:', width=lw, anchor='w').grid(row=rc, column=0)
+dbRWGb = tk.DoubleVar()
+dbRWGb.set(2)
+RWGb_entry = tk.Entry(rwgframe, textvariable=dbRWGb, width=ew)
+RWGb_entry.grid(row=rc, column=1)
+
+rwgbunit = tk.StringVar()
+rwgbunit.set(rwgaunits[2])
+ttk.Label(rwgframe, textvariable=rwgbunit, width=uw, anchor='w').grid(row=rc, column=2)
+
+rc += 1
+ttk.Label(rwgframe, text='Waveguide length:', width=lw, anchor='w').grid(row=rc, column=0)
+dbRWGlength = tk.DoubleVar()
+dbRWGlength.set(1)
+RWGlength_entry = tk.Entry(rwgframe, textvariable=dbRWGlength, width=ew)
+RWGlength_entry.grid(row=rc, column=1)
+
+rwgLunits = ["m", "dm", "cm", "mm", "ft", "in"]
+rwgLunits_combo = ttk.Combobox(rwgframe, values=rwgLunits, width=uw)
+rwgLunits_combo.current(0)
+rwgLunits_combo.grid(row=rc, column=2)
+
+rc += 1
+ttk.Label(rwgframe, text='Waveguide wall material:', width=lw, anchor='w').grid(row=rc, column=0)
+rwgMaterials = list(LC.WG_conductivity.keys())
+rwgMaterial_combo = ttk.Combobox(rwgframe, values=rwgMaterials, width=ew-3)
+rwgMaterial_combo.current(0)
+rwgMaterial_combo.grid(row=rc, column=1)
+rwgMaterial_combo.bind("<<ComboboxSelected>>", rwgMaterial_changed)
+
+rc += 1
+ttk.Label(rwgframe, text='Waveguide conductivity:', width=lw, anchor='w').grid(row=rc, column=0)
+dbRWGcond = tk.DoubleVar()
+dbRWGcond.set(LC.WG_conductivity["Aluminum"])
+RWGcond_entry = tk.Entry(rwgframe, textvariable=dbRWGcond, width=ew)
+RWGcond_entry.grid(row=rc, column=1)
+ttk.Label(rwgframe, text='S/m', width=uw, anchor='w').grid(row=rc, column=2)
+
+rc += 1
+ttk.Label(rwgframe, text='Waveguide filler material', width=lw, anchor='w').grid(row=rc, column=0)
+
+rc += 1
+ttk.Label(rwgframe, text='  Rel. permittivity:', width=lw, anchor='w').grid(row=rc, column=0)
+dbRWGepsr = tk.DoubleVar()
+dbRWGepsr.set(1)
+RWGepsr_entry = tk.Entry(rwgframe, textvariable=dbRWGepsr, width=ew)
+RWGepsr_entry.grid(row=rc, column=1)
+
+rc += 1
+ttk.Label(rwgframe, text='  Rel. permeability:', width=lw, anchor='w').grid(row=rc, column=0)
+dbRWGmur = tk.DoubleVar()
+dbRWGmur.set(1)
+RWGmur_entry = tk.Entry(rwgframe, textvariable=dbRWGmur, width=ew)
+RWGmur_entry.grid(row=rc, column=1)
+
+rc += 1
+ttk.Label(rwgframe, text='TEmn modes', width=lw, anchor='w').grid(row=rc, column=0)
+
+rc += 1
+ttk.Label(rwgframe, text='  m:', width=lw, anchor='w').grid(row=rc, column=0)
+intRWGTEm = tk.IntVar()
+intRWGTEm.set(1)
+RWGTEm_entry = tk.Entry(rwgframe, textvariable=intRWGTEm, width=ew)
+RWGTEm_entry.grid(row=rc, column=1)
+
+rc += 1
+ttk.Label(rwgframe, text='  n:', width=lw, anchor='w').grid(row=rc, column=0)
+intRWGTEn = tk.IntVar()
+intRWGTEn.set(0)
+RWGTEn_entry = tk.Entry(rwgframe, textvariable=intRWGTEn, width=ew)
+RWGTEn_entry.grid(row=rc, column=1)
+
+rc += 1
+rwgCalc_button = tk.Button(rwgframe, text="Calculate", command=calc_rwg, width=14)
+rwgCalc_button.grid(row=rc, column=0)
+
+rc = 0
+ttk.Label(rwgresframe, text='Recommended frequency:', width=lw, anchor='w').grid(row=rc, column=0)
+rwgRecomFreq = tk.StringVar()
+ttk.Label(rwgresframe, textvariable=rwgRecomFreq, width=lw, anchor='w').grid(row=rc, column=1)
+
+rc += 1
+ttk.Label(rwgresframe, text='Cut-off frequency:', width=lw, anchor='w').grid(row=rc, column=0)
+dbCutoffFreq = tk.DoubleVar()
+ttk.Label(rwgresframe, textvariable=dbCutoffFreq, width=lw, anchor='w').grid(row=rc, column=1)
+rwgfcunit = tk.StringVar()
+rwgfcunit.set("GHz")
+ttk.Label(rwgresframe, textvariable=rwgfcunit, width=uw, anchor='w').grid(row=rc, column=2)
+
+rc += 1
+ttk.Label(rwgresframe, text='Spec. attenuation:', width=lw, anchor='w').grid(row=rc, column=0)
+dbRWGatten = tk.DoubleVar()
+ttk.Label(rwgresframe, textvariable=dbRWGatten, width=lw, anchor='w').grid(row=rc, column=1)
+ttk.Label(rwgresframe, text='dB/m', width=uw, anchor='w').grid(row=rc, column=2)
+
+rc += 1
+ttk.Label(rwgresframe, text='Loss:', width=lw, anchor='w').grid(row=rc, column=0)
+dbRWGloss = tk.DoubleVar()
+ttk.Label(rwgresframe, textvariable=dbRWGloss, width=lw, anchor='w').grid(row=rc, column=1)
+ttk.Label(rwgresframe, text='dB', width=uw, anchor='w').grid(row=rc, column=2)
+
+
+# alfa, Lwg = LC.rectwaveguideloss(0.072136,0.034036,3.767e7, 2.6e9, 2.079406e9)
+
+""" MENUS """
+
+
+def plotrwgatten():
+    aunit = rwgaunits_combo.get()
+    a = dbRWGa.get() * LC.unit2SI[aunit]
+    b = dbRWGb.get() * LC.unit2SI[aunit]
+    m = intRWGTEm.get()
+    n = intRWGTEn.get()
+    mur = dbRWGmur.get()
+    epsr = dbRWGepsr.get()
+    sig = dbRWGcond.get()
+    frequnit = rwgfunits_combo.get()
+    fr = dbRWGfr.get() * LC.unit2SI[frequnit]
+    rwgl = dbRWGlength.get() * LC.unit2SI[rwgLunits_combo.get()]
+
+    fc = LC.rectwaveguide_cutofffreq(a, b, m, n, mur, epsr)
+    LC.plotrectwgatten(a, b, sig, fc, rwgType_combo.get(), rwgMaterial_combo.get())
+
+
+main_menu = tk.Menu(root)
+root.config(menu=main_menu)
+
+plot_menu = tk.Menu(main_menu, tearoff=False)
+main_menu.add_cascade(label="Plot", menu=plot_menu)
+plot_menu.add_command(label="Rectangular waveguide attenuation", command=plotrwgatten)
+
+root.mainloop()
